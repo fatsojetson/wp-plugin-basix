b7d4b04e5fec48873520b1ec8f5abf415904fd82
fatsojetson <38504195+fatsojetson@users.noreply.github.com>
2018-04-18 19:19:21 +0200

    Update mein-plugin.php

--- mein-plugin/mein-plugin.php
+++ mein-plugin/mein-plugin.php
@@ -1,11 +1,9 @@
 <?php
 /*
 Plugin Name: Mein erstes Plugin
-Plugin URI:
-Description: Dies ist mein erstes WordPress Plugin
+Description: Dies ist mein erstes WordPress Plugin.
 Version: 1.0
 Author: fatsojetson
-Author URI:
 License: GPLv2
 */
 ?>
