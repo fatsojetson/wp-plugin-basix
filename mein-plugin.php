bf3d7348cf5d5f27b76999b7b44cdaaa6662ae18
fatsojetson <38504195+fatsojetson@users.noreply.github.com>
2018-04-18 19:02:33 +0200

    Create mein-plugin.php

---
+++ mein-plugin.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Mein erstes Plugin
+Plugin URI: http://beispiel.de/plugins/mein-plugin
+Description: Dies ist mein erstes WordPress Plugin
+Version: 1.0
+Author: Max Mustermann
+Author URI: http://beispiel.de
+License: GPLv2
+*/
+?>
